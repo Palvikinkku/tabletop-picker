8eff8e306cc7f0dd403fb0b17a95ee096235bf15
Niko Leppänen <thepalvikinkku@gmail.com>
2018-06-04 15:14:22 +0300

    Initial Commit

---
+++ tabletop.js
@@ -0,0 +1,17 @@
+// Object full of boardgames
+
+const games = [
+    {name: 'Citadels', optimalPlayers: 4, maxPlayers: 8, difficulty: 'easy', length: 'short'},
+    {name: '7 Wonders Duel', optimalPlayers: 2, maxPlayers: 2, difficulty: 'medium', length: 'short'},
+    {name: 'Kankaanpään Kunkku', optimalPlayers: 3, maxPlayers: 6, difficulty: 'easy', length: 'long'},
+    {name: 'X-Com the Board Game', optimalPlayers: 4, maxPlayers: 4, difficulty: 'hard', length: 'long'},
+    {name: 'Smallworld', optimalPlayers: 4, maxPlayers: 5, difficulty: 'medium', length: 'long'},
+    {name: '7 Wonders', optimalPlayers: 4, maxPlayers: 7, difficulty: 'medium', length: 'short'},
+    {name: 'Qin', optimalPlayers: 4, maxPlayers: 4, difficulty: 'easy', length: 'medium'},
+    {name: 'Monopoly', optimalPlayers: 3, maxPlayers: 6, difficulty: 'easy', length: 'long'},
+    {name: 'Modern Art', optimalPlayers: 4, maxPlayers: 5, difficulty: 'easy', length: 'medium'},
+    {name: 'Rappa Kalja', optimalPlayers: 5, maxPlayers: 8, difficulty: 'easy', length: 'short'},
+    {name: 'Carcassonne', optimalPlayers: 3, maxPlayers: 7, difficulty: 'medium', length: 'medium'}
+];
+
+// Under here we create necessary filters so we can actually filter the necessary results
