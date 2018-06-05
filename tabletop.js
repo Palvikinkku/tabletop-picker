a5647b672b02159bd92b192cb76f3ba4cd315dea
Niko Leppänen <thepalvikinkku@gmail.com>
2018-06-05 14:20:05 +0300

    Created first working filter.

--- tabletop.js
+++ tabletop.js
@@ -14,4 +14,25 @@ const games = [
     {name: 'Carcassonne', optimalPlayers: 3, maxPlayers: 7, difficulty: 'medium', length: 'medium'}
 ];
 
+// Some fancy document element pickers
+
+let maxCheck = document.getElementById("maxCount"),
+    optimalCheck = document.getElementById("optimalCount"),
+    diffCheck = document.getElementById("difficultPicker"),
+    lengthCheck = document.getElementById("difficultPicker");
+
+let maxValue = maxCheck.options[maxCheck.selectedIndex].value,
+    optimalValue = optimalCheck.options[optimalCheck.selectedIndex].value,
+    difficultyValue = diffCheck.options[diffCheck.selectedIndex].value,
+    lengthValue = lengthCheck.options[lengthCheck.selectedIndex].value;
+
 // Under here we create necessary filters so we can actually filter the necessary results
+
+
+// Apparently the dropdown select is a string. Though the first filter does work, consider converting to number.
+// Also consider making functions later, especially when we have multiple filters running.
+for (i = 0; i < games.length; i++) {
+    if(games[i].maxPlayers == maxValue) {
+        console.log(games[i].name);
+    }
+}
